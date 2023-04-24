947b67b128ea56fa0f19a3902df6917623e67fd6
Sims- Kale <seemakale@reluconsultancy.in>
2023-04-24 22:39:28 +0530

    add city with tag_id

--- test.py
+++ test.py
@@ -24,49 +24,46 @@ def get_jwt_token(username, password):
     else:
         raise Exception(f"Failed to get JWT token. Status code: {response.status_code}. Response body: {response.text}")
     
-# def city(jwt_token, ws):
-#     city_url =' https://newbuildhomes.org/wp-json/wp/v2/property_city'
+def city(jwt_token, ws):
+    city_url =' https://newbuildhomes.org/wp-json/wp/v2/property_city'
   
-#     headers= {
-#         'Authorization': f'Bearer {jwt_token}',
-#         'Content-Type': 'application/json',
-#         'Accept-Encoding': 'gzip, deflate, br',
-#         'Connection': 'keep-alive',
-#         'Accept': '*/*',
-#         'User-Agent': 'Mozilla/5.0'
-#     }
-#     new_city = {
-#             'name': ws['C19'].value.split(',')[0]
-#         }
-#     print(new_city['name'])
+    headers= {
+        'Authorization': f'Bearer {jwt_token}',
+        'Content-Type': 'application/json',
+        'Accept-Encoding': 'gzip, deflate, br',
+        'Connection': 'keep-alive',
+        'Accept': '*/*',
+        'User-Agent': 'Mozilla/5.0'
+    }
+    new_city = {
+            'name': ws['C19'].value.split(',')[0]
+        }
+
     
    
-#     response = requests.post(url=city_url, headers=headers, json=new_city)
-#     print(response.text)
-#     if response.ok:
-#             print('New City added successfully!')
-#             city_id = json.load(response.text)
-#             print(city_id)
-#             return city_id['id']
-#     # else:
-#     #         print(f'Error while adding new city. Status code: {response.status_code}. Response body: {response.text}')
+    response = requests.post(url=city_url, headers=headers, json=new_city)
+   
+    if response.ok:
+            print('New City added successfully!')
+            city_id = json.load(response.text)
+            return city_id['id']
+    # else:
+    #         print(f'Error while adding new city. Status code: {response.status_code}. Response body: {response.text}')
     
-#     else:
-#         params= {
-#         'search': ws['C19'].value.split(',')[0]
-#     }
-#     print(params['search'])
+    else:
+        params= {
+        'search': ws['C19'].value.split(',')[0]
+    }
         
-#     response = requests.get(url=city_url, headers=headers, params=params)
-#     # print(response.text)
-#     if response.ok:
-#         response_data = json.loads(response.text)
-#         if response_data:
-#             city_id =response_data[0]['id']
-#             print(city_id)
-#             return city_id
-#         # else:
-#         #     raise Exception(f"No city found with name {params}")
+    response = requests.get(url=city_url, headers=headers, params=params)
+    # print(response.text)
+    if response.ok:
+        response_data = json.loads(response.text)
+        if response_data:
+            city_id =response_data[0]['id']
+            return city_id
+        # else:
+        #     raise Exception(f"No city found with name {params}")
        
     
 
@@ -84,7 +81,7 @@ def create_post(jwt_token, post_url, ws, city_id):
     post_data = {
         'title': ws['B19'].value,
         'status': 'publish',
-        # 'property_city': city_id
+        'property_city': city_id
         
     }
     response = requests.post(url=post_url, headers=headers, json=post_data)
@@ -103,7 +100,7 @@ def main():
     ws = wb['extraction results']
 
     # city(jwt_token,json_str)
-    # id = city(jwt_token, ws)
+    id = city(jwt_token, ws)
     create_post(jwt_token, post_url, ws, id)
     
 
