157cec3657142cf3b820442c0307951ac2129a57
Sims- Kale <seemakale@reluconsultancy.in>
2023-04-25 13:59:07 +0530

    Post Created with titl, city & type

--- test.py
+++ test.py
@@ -22,12 +22,14 @@ def get_jwt_token(username, password):
         response_data = json.loads(response.text)
         return response_data['token']
     else:
-        raise Exception(f"Failed to get JWT token. Status code: {response.status_code}. Response body: {response.text}")
-    
+        raise Exception(
+            f"Failed to get JWT token. Status code: {response.status_code}. Response body: {response.text}")
+
+
 def city(jwt_token, ws):
-    city_url =' https://newbuildhomes.org/wp-json/wp/v2/property_city'
-  
-    headers= {
+    city_url = ' https://newbuildhomes.org/wp-json/wp/v2/property_city'
+
+    headers = {
         'Authorization': f'Bearer {jwt_token}',
         'Content-Type': 'application/json',
         'Accept-Encoding': 'gzip, deflate, br',
@@ -36,35 +38,36 @@ def city(jwt_token, ws):
         'User-Agent': 'Mozilla/5.0'
     }
     new_city = {
-            'name': ws['C24'].value.split(',')[0]
-        }
+        'name': ws['C24'].value.split(',')[0]
+    }
     response = requests.post(url=city_url, headers=headers, json=new_city)
-   
+
     if response.ok:
-            print('New City added successfully!')
-            city_id = json.load(response.text)
-            return city_id['id']
+        print('New City added successfully!')
+        city_id = json.load(response.text)
+        return city_id['id']
     # else:
     #         print(f'Error while adding new city. Status code: {response.status_code}. Response body: {response.text}')
-    
+
     else:
-        params= {
-        'search': ws['C24'].value.split(',')[0]
-    }
-        
+        params = {
+            'search': ws['C24'].value.split(',')[0]
+        }
+
     response = requests.get(url=city_url, headers=headers, params=params)
     if response.ok:
         response_data = json.loads(response.text)
         if response_data:
-            city_id =response_data[0]['id']
+            city_id = response_data[0]['id']
             return city_id
         # else:
         #     raise Exception(f"No city found with name {params}")
-       
+
+
 def Type(jwt_token, ws):
     type_url = 'https://newbuildhomes.org/wp-json/wp/v2/property_type'
     print(type_url)
-    headers= {
+    headers = {
         'Authorization': f'Bearer {jwt_token}',
         'Content-Type': 'application/json',
         'Accept-Encoding': 'gzip, deflate, br',
@@ -74,7 +77,7 @@ def Type(jwt_token, ws):
     }
     new_type = {
         'name': ws['D24'].value
-        }
+    }
     response = requests.post(url=type_url, headers=headers, json=new_type)
     # print(response.text)
     type = json.loads(response.text)
@@ -84,34 +87,13 @@ def Type(jwt_token, ws):
         print(type_id)
         return type_id
     else:
-        type_id= type['data']['term_id']
-        print( type_id)
+        type_id = type['data']['term_id']
+        print(type_id)
         return type_id
-    
-        
-        # print(type_id['id']
-     
-    # else:
-
-    #     params= {
-    #     'search': (type_id,{'term_id'})
-    #     }
-    # print(params['search'])
-        
-    # response = requests.get(url=type_url, headers=headers, params=params)
-    # if response.ok:
-    #     response_data = json.loads(response.text)
-    #     if response_data:
-    #         type_id =response_data[0]['id']
-    #         print(type_id)
-    #         return type_id
-    
-
-    
 
 
 def create_post(jwt_token, post_url, ws, city_id, type_id):
-    
+
     headers = {
         'Authorization': f'Bearer {jwt_token}',
         'Content-Type': 'application/json',
@@ -125,13 +107,15 @@ def create_post(jwt_token, post_url, ws, city_id, type_id):
         'status': 'publish',
         'property_city': city_id,
         'property_type': type_id
-        
+
     }
     response = requests.post(url=post_url, headers=headers, json=post_data)
     if response.ok:
         print('Post created successfully!')
     else:
-        print(f'Error creating post. Status code: {response.status_code}. Response body: {response.text}')
+        print(
+            f'Error creating post. Status code: {response.status_code}. Response body: {response.text}')
+
 
 def main():
     username = 'Muktesh'
@@ -146,7 +130,7 @@ def main():
     id = city(jwt_token, ws)
     type = Type(jwt_token, ws)
     create_post(jwt_token, post_url, ws, id, type)
-    
+
 
 if __name__ == '__main__':
     main()
