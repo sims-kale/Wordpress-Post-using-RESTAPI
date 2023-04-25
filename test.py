e65e33ae323c260e00d6f9519b2077ed1e526f4e
Sims- Kale <seemakale@reluconsultancy.in>
2023-04-25 13:44:40 +0530

    Property Type added Successfully

--- test.py
+++ test.py
@@ -36,11 +36,8 @@ def city(jwt_token, ws):
         'User-Agent': 'Mozilla/5.0'
     }
     new_city = {
-            'name': ws['C19'].value.split(',')[0]
+            'name': ws['C24'].value.split(',')[0]
         }
-
-    
-   
     response = requests.post(url=city_url, headers=headers, json=new_city)
    
     if response.ok:
@@ -52,11 +49,10 @@ def city(jwt_token, ws):
     
     else:
         params= {
-        'search': ws['C19'].value.split(',')[0]
+        'search': ws['C24'].value.split(',')[0]
     }
         
     response = requests.get(url=city_url, headers=headers, params=params)
-    # print(response.text)
     if response.ok:
         response_data = json.loads(response.text)
         if response_data:
@@ -65,10 +61,56 @@ def city(jwt_token, ws):
         # else:
         #     raise Exception(f"No city found with name {params}")
        
+def Type(jwt_token, ws):
+    type_url = 'https://newbuildhomes.org/wp-json/wp/v2/property_type'
+    print(type_url)
+    headers= {
+        'Authorization': f'Bearer {jwt_token}',
+        'Content-Type': 'application/json',
+        'Accept-Encoding': 'gzip, deflate, br',
+        'Connection': 'keep-alive',
+        'Accept': '*/*',
+        'User-Agent': 'Mozilla/5.0'
+    }
+    new_type = {
+        'name': ws['D24'].value
+        }
+    response = requests.post(url=type_url, headers=headers, json=new_type)
+    # print(response.text)
+    type = json.loads(response.text)
+    if response.ok:
+        print("Property Type added successfully")
+        type_id = type['id']
+        print(type_id)
+        return type_id
+    else:
+        type_id= type['data']['term_id']
+        print( type_id)
+        return type_id
+    
+        
+        # print(type_id['id']
+     
+    # else:
+
+    #     params= {
+    #     'search': (type_id,{'term_id'})
+    #     }
+    # print(params['search'])
+        
+    # response = requests.get(url=type_url, headers=headers, params=params)
+    # if response.ok:
+    #     response_data = json.loads(response.text)
+    #     if response_data:
+    #         type_id =response_data[0]['id']
+    #         print(type_id)
+    #         return type_id
+    
+
     
 
 
-def create_post(jwt_token, post_url, ws, city_id):
+def create_post(jwt_token, post_url, ws, city_id, type_id):
     
     headers = {
         'Authorization': f'Bearer {jwt_token}',
@@ -81,7 +123,8 @@ def create_post(jwt_token, post_url, ws, city_id):
     post_data = {
         'title': ws['B19'].value,
         'status': 'publish',
-        'property_city': city_id
+        'property_city': city_id,
+        'property_type': type_id
         
     }
     response = requests.post(url=post_url, headers=headers, json=post_data)
@@ -101,7 +144,8 @@ def main():
 
     # city(jwt_token,json_str)
     id = city(jwt_token, ws)
-    create_post(jwt_token, post_url, ws, id)
+    type = Type(jwt_token, ws)
+    create_post(jwt_token, post_url, ws, id, type)
     
 
 if __name__ == '__main__':
