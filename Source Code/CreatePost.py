418c00e1e01cb73d2462f0ad151225ed32c36103
Sims- Kale <simskale122@gmail.com>
2023-05-03 18:39:03 +0530

    Post Updated with Address, Map, Zipcode & Map

--- Source Code/CreatePost.py
+++ Source Code/CreatePost.py
@@ -1,4 +1,5 @@
 import re
+import tempfile
 import requests
 from requests_toolbelt.multipart.encoder import MultipartEncoder
 import os
@@ -10,7 +11,7 @@ import logging
 from datetime import datetime, timedelta
 
 
-logging.basicConfig(filename='Source Code/Logs.log', level=logging.INFO)
+logging.basicConfig(filename='Source Code/Posts.log', level=logging.INFO)
 
 
 def get_jwt_token(username, password):
@@ -64,6 +65,38 @@ def City(jwt_token, city_name):
         city_id = city['data']['term_id']
         logging.info("City already existed: " + str(city_id))
         return city_id
+    
+def Area(jwt_token, area_name):
+    if area_name is None:
+        logging.info("Area name is None, skipping...")
+        return None
+    logging.info("Property Area: " + area_name)
+
+    area_url = ' https://newbuildhomes.org/wp-json/wp/v2/property_area'
+
+    headers = {
+        'Authorization': f'Bearer {jwt_token}',
+        'Content-Type': 'application/json',
+        'Accept-Encoding': 'gzip, deflate, br',
+        'Connection': 'keep-alive',
+        'Accept': '*/*',
+        'User-Agent': 'Mozilla/5.0'
+    }
+    new_area = {
+        'name': area_name
+    }
+
+    response = requests.post(url=area_url, headers=headers, json=new_area)
+    area = json.loads(response.text)
+    if response.ok:
+        area_id = area['id']
+        logging.info("Property area added successfully " + str(area_id))
+
+        return area_id
+    else:
+        area_id = area['data']['term_id']
+        logging.info("area already existed: " + str(area_id))
+        return area_id
 
 
 def Type(jwt_token, typestring):
@@ -72,7 +105,7 @@ def Type(jwt_token, typestring):
         logging.info("Typestring is None, skipping...")
         return None
     
-    logging.info("Property Type: " + typestring)
+    logging.info("Property Type: " + typestring.strip())
 
     type_url = 'https://newbuildhomes.org/wp-json/wp/v2/property_type'
 
@@ -125,82 +158,74 @@ def Type(jwt_token, typestring):
                 logging.info("Property type already existed: " + str(child_type_id))
                 types.append(int(child_type_id))
     
-    print(types)
+    # print(types)
     return types
 
 
-# def Media(jwt_token, imageurl):
+def Media(jwt_token, arr_images):
+    print(arr_images)
 
-#     if not imageurl:
-#         logging.info("Image URL is empty, skipping...")
-#         return None
-
-#     media_url = "https://newbuildhomes.org/wp-json/wp/v2/media"
-
-#     raw = requests.get(imageurl).content
-
-#     file = tempfile.NamedTemporaryFile(delete=False, mode="wb", suffix=".jpeg")
-#     filename = file.name
-#     print(filename)
-
-#     with file as img:
-#         img.write(raw)
-
-#     fileName = os.path.basename(filename)
-#     multipart_data = MultipartEncoder(
-#         fields={
-#             # a file upload field
-#             'file': (fileName, open(filename, 'rb'), 'image/jpeg'),
-#             # plain text fields
-#             'alt_text': 'alt test',
-#             'caption': 'caption test',
-#             'description': 'description test',
-#         }
-#     )
-
-#     headers = {
-#         'Authorization': f'Bearer {jwt_token}',
-#         'Content-Type': multipart_data.content_type,
-#         'Accept-Encoding': 'gzip, deflate, br',
-#         'Connection': 'keep-alive',
-#         'Content-Disposition': 'attachment; filename="1.1.jpeg"',
-#         'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
-#         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/112.0'
-#     }
-
-#     response = requests.post(
-#         url=media_url, headers=headers, data=multipart_data)
-
-#     # print(response.text)
-
-#     media = json.loads(response.text)
-
-#     # print(media)
-
-#     if response.ok:
-
-#         media_id = media['id']
-#         logging.info("Property images added successfully "+ str(media_id))
-
-#         # res = requests.post(
-#         #     'https://newbuildhomes.org/wp-json/wp/v2/properties/18225',
-#         #     headers=headers,
-#         #     json=[media_id]
-#         # )
-#         # print(res.text)
-#         # print(media_id)
-#         return (media_id)
-#     # else:
-#     #     media_id = media['data']['term_id']
-#     #     logging.info("Image already existed: "+media_id)
-#     #     return media_id
-
-
-def create_post(jwt_token, post_url, title, city_id, type_id):
-    if title is None:
-        logging.info("Title is None, skipping...")
+    if not arr_images:
+        logging.info("Image URL is empty, skipping...")
         return None
-    logging.info("Property Title: "+title)
+
+    media_url = "https://newbuildhomes.org/wp-json/wp/v2/media"
+
+    if os.path.isfile(arr_images):
+        with open(arr_images, "rb") as f:
+            raw = f.read()
+            file = tempfile.NamedTemporaryFile(delete=False, mode="wb", suffix=".jpeg")
+            filename = file.name
+            with file as img:
+                img.write(raw)
+
+    fileName = os.path.basename(filename)
+    multipart_data = MultipartEncoder(
+        fields={
+            # a file upload field
+            'file': (fileName, open(filename, 'rb'), 'image/jpeg'),
+            # plain text fields
+            'alt_text': 'alt test',
+            'caption': 'caption test',
+            'description': 'description test',
+        }
+    )
+
+    headers = {
+        'Authorization': f'Bearer {jwt_token}',
+        'Content-Type': multipart_data.content_type,
+        'Accept-Encoding': 'gzip, deflate, br',
+        'Connection': 'keep-alive',
+        'Content-Disposition': 'attachment; filename="1.1.jpeg"',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/112.0'
+    }
+
+    response = requests.post(
+        url=media_url, headers=headers, data=multipart_data)
+
+    # print(response.text)
+
+    media = json.loads(response.text)
+
+    # print(media)
+
+    if response.ok:
+
+        media_id = media['id']
+        logging.info("Property images added successfully "+ str(media_id))
+        return (media_id)
+    # else:
+    #     media_id = media['data']['term_id']
+    #     logging.info("Image already existed: "+media_id)
+    #     return media_id
+
+
+def create_post(jwt_token, post_url, title, city_id, area_id, type_id, media_id):
+    # if title is None:
+    #     logging.info("Title is None, skipping...")
+    #     return None
+    # logging.info("Property Title: "+title)
 
     headers = {
         'Authorization': f'Bearer {jwt_token}',
@@ -213,11 +238,11 @@ def create_post(jwt_token, post_url, title, city_id, type_id):
     # for row in ws.iter_rows(min_row=2, max_row=2, min_col=1, values_only=True):
     post_data = {
         'title': title,
-        'fave_property_price': 12345,
-        'status': 'publish',
         'property_city': city_id,
+        'property_area': area_id,
         'property_type': type_id,
-        # 'featured_media': media_id
+        'featured_media': media_id,
+        'status': 'publish',
 
     }
     response = requests.post(url=post_url, headers=headers, json=post_data)
@@ -246,22 +271,37 @@ def main():
 
     wb = openpyxl.load_workbook('Property.xlsx')
     ws = wb['extraction results']
+    
+    for row in ws.iter_rows(min_row=2, max_row=4, min_col=1, values_only=True):
 
-    for row in ws.iter_rows(min_row=469, max_row=469, min_col=1, values_only=True):
-        title = row[1]
+        title = row[1].split("by")[0]
+        if title is None:
+            logging.info("Title is None, skipping...")
+            return None
+        logging.info("Property Title: "+title)
 
         city_name = row[2].split(',')[0]
-
+        area_name = row[2].split(',')[1]
         typestring = row[3]
-
-        # arr_images = row[6].replace("]", "").replace(
-        #     "[", "").replace("'", "").split(",")[0]
+        arr_images =  row[5].replace("]", "").replace("[", "").replace("'", "").split(",")[0]  #update Media
 
         city = City(jwt_token, city_name)
+         
+        area = Area(jwt_token, area_name)
         
         type = Type(jwt_token, typestring)
+
+        media = Media(jwt_token, arr_images)
+
+               
+        create_post(jwt_token, post_url, title, city, area, type, media)
+    
         
-        create_post(jwt_token, post_url, title, city, type)
+        # ws.cell(row=2, column=9, value=post_url)
+        
+
+        # wb.save('Property.xlsx')
+
 
 
 if __name__ == '__main__':
--- Source Code/Selenium.py
+++ Source Code/Selenium.py
@@ -1,3 +1,4 @@
+import logging
 import re
 import openpyxl
 from selenium.webdriver import Chrome, ChromeOptions
@@ -9,6 +10,8 @@ from selenium.webdriver.common.action_chains import ActionChains
 import autoit
 import time
 
+logging.basicConfig(filename='Source Code/Selenium.log', level=logging.INFO)
+
 def login(driver, username, password):
     driver.get("https://newbuildhomes.org/wp-login.php")
     username_elem = driver.find_element(By.ID, "user_login")
@@ -17,7 +20,7 @@ def login(driver, username, password):
     username_elem.send_keys(username)
     password_elem.send_keys(password)
     login_button.click()
-    print("Login Successful")
+    logging.info("Login Successful")
     
     
 
@@ -27,10 +30,11 @@ def upload_media(driver, media):
     media_tab.click()
     time.sleep(2)
     for image in media:
-        wait = WebDriverWait(driver, 10)
+        logging.info("Image Path: " + image)
+        wait = WebDriverWait(driver, 7)
         # backdrop = wait.until_not(EC.visibility_of_element_located((By.CSS_SELECTOR, '.media-modal-backdrop')))
-        add_media_button = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="houzez-property-meta-box"]/div[2]/div/div/div/div[4]/div[1]/div/div/div[2]/div/div[2]/a')))
-        time.sleep(8)
+        add_media_button = driver.find_elements(By.XPATH, '//*[@id="houzez-property-meta-box"]/div[2]/div/div/div/div[4]/div[1]/div/div/div[2]/div/div[2]/a')[0]
+        # driver.execute_script("arguments[0].click();", add_media_button)
         add_media_button.click()
         time.sleep(2)
         upload_file_button = driver.find_elements(By.CSS_SELECTOR, '#menu-item-upload')[-1]
@@ -39,53 +43,56 @@ def upload_media(driver, media):
         select_files_button = driver.find_elements(By.CSS_SELECTOR, '.wp-core-ui .button-group.button-hero .button, .wp-core-ui .button.button-hero')[-1]
         select_files_button.click()
         autoit.win_wait_active("Open")
+        time.sleep(3)
         autoit.control_set_text("Open", "Edit1", image)
-        time.sleep(1)
+        time.sleep(3)
         autoit.control_send("Open", "Edit1", "{ENTER}")
-        time.sleep(7)
+        time.sleep(5)
         select_button = driver.find_elements(By.XPATH, '//button[@class="button media-button button-primary button-large media-button-select"]')[-1]
         if select_button.is_enabled():
+            time.sleep(3)
             select_button.click()
         else:
-            time.sleep(10)
+            time.sleep(60)
             select_button.click()
-        time.sleep(5)
-    # update_button = driver.find_element(By.ID, "publish")
-    # update_button.click()
+    logging.info("Media Uploaded")
+    time.sleep(30)
 
-    print("Media Uploaded")
-
-def update_price(driver, primary_price, secondary_price):
-    # wait = WebDriverWait(driver, 10)
-
-    # info = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="houzez-property-meta-box"]/div[2]/div/div/ul/li[1]/a')))
-    info = driver.find_element(By.XPATH,'//*[@id="houzez-property-meta-box"]/div[2]/div/div/ul/li[1]/a')
+def update_price(driver, price, primary_price, secondary_price):
+    if price is None:
+        logging.info("Prices are None, skipping...")
+        return None
+    logging.info("Prices: " + price)
+    wait = WebDriverWait(driver, 30)
+    info = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="houzez-property-meta-box"]/div[2]/div/div/ul/li[1]/a')))
+    # info = driver.find_element(By.XPATH,'//*[@id="houzez-property-meta-box"]/div[2]/div/div/ul/li[1]/a')
     driver.execute_script("arguments[0].click();", info)
     # wait.until(EC.invisibility_of_element_located((By.XPATH, '//a[@href="https://newbuildhomes.org/"]')))
-    # time.sleep(5)
+    time.sleep(15)
     info.click()
 
     enter_price = driver.find_element(By.ID, 'fave_property_price')
     enter_price.clear()
     time.sleep(2)
     enter_price.send_keys(str(primary_price))
-    print("Enter primary price:", primary_price)
+    logging.info("Enter primary price:", primary_price)
   
     enter_2price = driver.find_element(By.ID, 'fave_property_sec_price')
     enter_2price.clear()
     enter_2price.send_keys(str(secondary_price))
-    print("Enter secondary price:", secondary_price)
+    logging.info("Enter secondary price:", secondary_price)
     time.sleep(1)
-    update_button = driver.find_element(By.ID, "publish")
-    update_button.click()
     time.sleep(5)
-    print("Price Updated")
+    logging.info("Price Updated")
 
 def developer(driver, dev_names):
-    print(dev_names)
-    wait = WebDriverWait(driver, 5)
+    if dev_names is None:
+        logging.info("Developer names is None, skipping...")
+        return None
+    logging.info("Developer Names: " + dev_names)
+    wait = WebDriverWait(driver, 30)
     contact_button = wait.until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[1]/div[2]/div[3]/div[1]/div[3]/form/div[2]/div/div[3]/div[1]/div[1]/div[2]/div/div/ul/li[6]/a')))
-    time.sleep(2)
+    time.sleep(15)
     contact_button.click()
     time.sleep(2)
     radio_button= wait.until(EC.element_to_be_clickable((By.XPATH,'/html/body/div[1]/div[2]/div[3]/div[1]/div[3]/form/div[2]/div/div[3]/div[1]/div[1]/div[2]/div/div/div/div[6]/div[1]/div/div/div[2]/ul/li[3]/label/input')))
@@ -101,12 +108,49 @@ def developer(driver, dev_names):
     slelect_result= driver.find_element(By.XPATH, '//*[@id="select2-fave_property_agency-results"]')
     time.sleep(2)
     slelect_result.click() 
-    update_button = driver.find_element(By.ID, "publish")
-    update_button.click()
     time.sleep(5)
+
+def Map(driver, address, zipcode):
+    wait = WebDriverWait(driver, 15)
+    map_button = wait.until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[1]/div[2]/div[3]/div[1]/div[3]/form/div[2]/div/div[3]/div[1]/div[1]/div[2]/div/div/ul/li[2]/a')))
+    time.sleep(15)
+    map_button.click()
+    input= driver.find_element(By.XPATH, '//*[@id="fave_property_map_address"]')
+    time.sleep(2)
+    input.click()
+    input.clear()
+    input.send_keys(zipcode)
+    time.sleep(3)
+    select_field= driver.find_element(By.XPATH,'//ul[@class="ui-menu ui-widget ui-widget-content ui-autocomplete ui-front"]')
+    option= select_field.find_element(By.XPATH,'.//li')
+    option.click()    
+    time.sleep(2)  
+# Property Setting
+    wait = WebDriverWait(driver, 10)
+    adress_button = wait.until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[1]/div[2]/div[3]/div[1]/div[3]/form/div[2]/div/div[3]/div[1]/div[1]/div[2]/div/div/ul/li[3]/a')))
+    adress_button.click() 
+    time.sleep(2)
+
+    street= driver.find_element(By.XPATH,'//*[@id="fave_property_address"]')
+    time.sleep(3)
+    street.click()
+    street.clear()
+    street.send_keys(address)
+    time.sleep(3)
+    zip=driver.find_element(By.XPATH,'//*[@id="fave_property_zip"]')
+    time.sleep(3)
+    zip.click()
+    zip.clear()
+    zip.send_keys(zipcode)
+    time.sleep(2)
+
+    logging.info("Address updated")
+    
+
 
 
 def main():
+    
     service = Service("C:/Drivers/chromedriver.exe")
     options = ChromeOptions()
     options.add_experimental_option('excludeSwitches', ['enable-logging'])
@@ -117,30 +161,39 @@ def main():
     username = "Muktesh"
     password = "2d6cHuKWGAxU0OOpHJ4yrsem"
     login(driver, username, password)
-
-
-    # Upload media
-    wb = openpyxl.load_workbook('Property.xlsx')
+                                                                                                            
+    wb = openpyxl.load_workbook('Property.xlsx')                                                          # Upload media
     ws = wb['extraction results']
     for row in ws.iter_rows(min_row=4, max_row=4, min_col=1, values_only=True):
         media = row[5].replace("]", "").replace("[", "").replace("'", "").replace("/","\\").split(",")   #update Media
-        # for i in range(3):
-        # post_id = 24732 + i*2
-        property_update_url = f"https://newbuildhomes.org/wp-admin/post.php?post=24732&action=edit"
-        # print(url)
-        driver.get(property_update_url)
-        # print(f"Opened post {post_id}")
         
-        # print(dev_names)
-        # upload_media(driver, media)
+       
+        property_update_url = row[7]
+        print(type(property_update_url))
+        print(property_update_url)
+        logging.info("update URL: ", str(property_update_url))
+        
+        driver.get(property_update_url)
+        
+        upload_media(driver, media)
         price = row[4]                                                                                   #update Media
         primary_price = int(re.search("£([\d,]+)", price).group(1).replace(",", ""))
         secondary_price_match = re.search("- £([\d,]+)", price)
         secondary_price = int(secondary_price_match.group(1).replace(",", "")) if secondary_price_match else ('')
-        # update_price(driver, primary_price, secondary_price)
+        update_price(driver,price, primary_price, secondary_price)
         dev_names= row[1].split("by")[1]                                                                  #Update Developer
         developer(driver, dev_names)
-    driver.quit()
+        address = row[2]
+        zipcode = row[2].split(",")[2]
+        logging.info("Address"+ address)
+        Map(driver,address, zipcode)
+
+        update_button = driver.find_element(By.ID, "publish")
+        update_button.click()
+        time.sleep(10)
+        logging.info("Post Updated !")
+        logging.info("-----------------------------------------------------------------------------")
+        driver.quit()
 
 if __name__ == "__main__":
     main()
